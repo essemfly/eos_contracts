c98a3d1a2f2f281705fc47577490405d950870fd
seokmin <seokmin@chain.partners>
2018-09-22 15:17:19 +0100

    Initial front end vuejs settings

--- my_world/src/App.vue
+++ my_world/src/App.vue
@@ -1,19 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import Eos from './components/Eos.vue';
-
 export default {
   name: 'app',
   components: {
-    HelloWorld,
-    Eos,
   },
 };
 </script>
